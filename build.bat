3ab42de9678a2c0bc8999c92798260a062a20449
hungnm <jokerhung@gmail.com>
2025-08-06 15:10:31 +0700

    update script

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 pyinstaller --noconfirm --onedir --console ^
---icon "C:\Users\hungnm\Downloads\image (2).ico" ^
+--icon ".\mydll\anpr.ico" ^
 --name "ALPRFAST" ^
---add-binary "D:\jkhung\work\vetc\github\ANPR_YoloV11_VietOCR\mydll\*.dll;." ^
---add-data "D:\jkhung\work\vetc\github\ANPR_YoloV11_VietOCR\config.json;." ^
-"D:\jkhung\work\vetc\github\ANPR_YoloV11_VietOCR\ocr_server_fastanpr.py"
+--add-binary ".\mydll\*.dll;." ^
+--add-data ".\config.json;." ^
+".\ocr_server_fastanpr_combine.py"
 copy "dist\ALPRFAST\_internal\config.json" "dist\ALPRFAST\config.json"
---
+++ run_hide.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -WindowStyle Hidden -Command "Start-Process 'ALPRFAST.exe' -WindowStyle Hidden"
