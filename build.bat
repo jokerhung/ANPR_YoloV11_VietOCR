7640d4b23f27c509df4f8cbe4be3b56462ef80cb
hungnm <jokerhung@gmail.com>
2025-08-06 15:19:35 +0700

    update script

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@ pyinstaller --noconfirm --onedir --console ^
 --name "ALPRFAST" ^
 --add-binary ".\mydll\*.dll;." ^
 --add-data ".\config.json;." ^
+--add-data ".\run.bat;." ^
 ".\ocr_server_fastanpr_combine.py"
 copy "dist\ALPRFAST\_internal\config.json" "dist\ALPRFAST\config.json"
+copy "dist\ALPRFAST\_internal\run.bat" "dist\ALPRFAST\run.bat"
